165cfb808fa0eebf975591ccb31b123015caa57e
Danil Korennykh <bluskript@gmail.com>
2021-02-21 13:11:54 -0500

    i am going insane

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.6.3
+	github.com/pion/rtcp v1.2.6
 	github.com/pion/webrtc/v2 v2.2.26 // indirect
 	github.com/pion/webrtc/v3 v3.0.2
 	github.com/thanhpk/randstr v1.0.4
--- main.go
+++ main.go
@@ -2,34 +2,31 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"sync"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/pion/rtcp"
 	"github.com/pion/webrtc/v3"
 	"github.com/thanhpk/randstr"
 )
 
-// type VoiceChannel struct {
-// 	*sync.RWMutex
-// 	Tracks map[string]webrtc.TrackLocal
-// 	Users  map[string]*webrtc.PeerConnection
-// }
+type VoiceChannel struct {
+	sync.RWMutex
+	Tracks map[string]webrtc.TrackLocal
+	Users  map[string]*webrtc.PeerConnection
+}
 
-// var channel = VoiceChannel{
-// 	Tracks:  make(map[string]webrtc.TrackLocal),
-// 	Users:   make(map[string]*webrtc.PeerConnection),
-// 	RWMutex: &sync.RWMutex{},
-// }
-
-var outTrack webrtc.TrackLocal
-var clients = make(map[string]*webrtc.PeerConnection)
-var trackMut = sync.RWMutex{}
+var channel = VoiceChannel{
+	Tracks:  make(map[string]webrtc.TrackLocal),
+	Users:   make(map[string]*webrtc.PeerConnection),
+	RWMutex: sync.RWMutex{},
+}
 
 var peerConnectionConfig = webrtc.Configuration{
 	ICEServers: []webrtc.ICEServer{
@@ -39,91 +36,108 @@ var peerConnectionConfig = webrtc.Configuration{
 	},
 }
 
-func SDPHandler(c *gin.Context) {
-	reqUserID := randstr.Hex(16)
-	trackMut.Lock()
-	defer trackMut.Unlock()
+func readSessionDescription(body io.ReadCloser) (*webrtc.SessionDescription, error) {
+	read, err := ioutil.ReadAll(body)
+	if err != nil {
+		return nil, err
+	}
 
-	body, err := ioutil.ReadAll(c.Request.Body)
+	offer := webrtc.SessionDescription{}
+	if err := json.Unmarshal(read, &offer); err != nil {
+		fmt.Println("error parsing SDP", err)
+		return nil, err
+	}
+	return &offer, nil
+}
+
+func newPeer(userID string) (*webrtc.PeerConnection, error) {
+	peerConnection, err := webrtc.NewPeerConnection(peerConnectionConfig)
+	if err != nil {
+		return nil, err
+	}
+	channel.Users[userID] = peerConnection
+	if _, err := peerConnection.AddTransceiverFromKind(webrtc.RTPCodecTypeAudio, webrtc.RTPTransceiverInit{
+		Direction: webrtc.RTPTransceiverDirectionSendrecv,
+	}); err != nil {
+		return nil, err
+	}
+	return peerConnection, nil
+}
+
+func SDPHandler(c *gin.Context) {
+	// generate a new random ID for this track
+	reqUserID := randstr.Hex(16)
+	// no race conditions
+	channel.Lock()
+	defer channel.Unlock()
+
+	// parses the SessionDescription from the request
+	offer, err := readSessionDescription(c.Request.Body)
 	if err != nil {
 		c.Status(http.StatusUnprocessableEntity)
 		return
 	}
 
-	offer := webrtc.SessionDescription{}
-	if err := json.Unmarshal(body, &offer); err != nil {
-		fmt.Println("error parsing SDP", err)
-		c.Status(http.StatusBadRequest)
+	// create a new peerConnection
+	peerConnection, err := newPeer(reqUserID)
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
 		return
 	}
 
-	peerConnection, err := webrtc.NewPeerConnection(peerConnectionConfig)
-	if err != nil {
-		panic(err)
-	}
-	clients[reqUserID] = peerConnection
-	if outTrack == nil {
-		// Allow us to receive 1 audio track
-		_, err = peerConnection.AddTransceiverFromKind(webrtc.RTPCodecTypeAudio)
-		if err != nil {
-			panic(err)
-		}
-
-		peerConnection.OnTrack(func(remoteTrack *webrtc.TrackRemote, receiver *webrtc.RTPReceiver) {
-			localTrack, newTrackErr := webrtc.NewTrackLocalStaticRTP(remoteTrack.Codec().RTPCodecCapability, "audio", "pion")
-			if newTrackErr != nil {
-				panic(newTrackErr)
-			}
-			outTrack = localTrack
-
-			trackMut.Lock()
-			for _, conn := range clients {
-				rtpSender, err := conn.AddTrack(outTrack)
-				if err != nil {
-					panic(err)
-				}
-
-				go func() {
-					rtcpBuf := make([]byte, 1500)
-					for {
-						if _, _, rtcpErr := rtpSender.Read(rtcpBuf); rtcpErr != nil {
-							return
-						}
-					}
-				}()
-			}
-			trackMut.Unlock()
-
-			rtpBuf := make([]byte, 1400)
-			for {
-				i, _, readErr := remoteTrack.Read(rtpBuf)
-				if readErr != nil {
-					panic(readErr)
-				}
-
-				// ErrClosedPipe means we don't have any subscribers, this is ok if no peers have connected yet
-				if _, err = localTrack.Write(rtpBuf[:i]); err != nil && !errors.Is(err, io.ErrClosedPipe) {
-					panic(err)
-				}
-			}
-		})
-	} else {
-		rtpSender, err := peerConnection.AddTrack(outTrack)
-		if err != nil {
-			panic(err)
-		}
-
+	peerConnection.OnTrack(func(remoteTrack *webrtc.TrackRemote, r *webrtc.RTPReceiver) {
+		// send a picture loss indication so a keyframe is pushed
 		go func() {
-			rtcpBuf := make([]byte, 1500)
-			for {
-				if _, _, rtcpErr := rtpSender.Read(rtcpBuf); rtcpErr != nil {
-					return
+			ticker := time.NewTicker(time.Second * 3)
+			for range ticker.C {
+				if rtcpSendErr := peerConnection.WriteRTCP([]rtcp.Packet{&rtcp.PictureLossIndication{MediaSSRC: uint32(remoteTrack.SSRC())}}); rtcpSendErr != nil {
+					fmt.Println(rtcpSendErr)
 				}
 			}
 		}()
+
+		// make a local track that feeds into all clients
+		localTrack, err := webrtc.NewTrackLocalStaticRTP(remoteTrack.Codec().RTPCodecCapability, remoteTrack.ID(), remoteTrack.StreamID())
+		if err != nil {
+			return
+		}
+
+		// add the track and add it to all existing clients
+		channel.Lock()
+		channel.Tracks[remoteTrack.ID()] = localTrack
+		for _, track := range channel.Tracks {
+			for _, conn := range channel.Users {
+				if _, err := conn.AddTrack(track); err != nil {
+					panic(err)
+				}
+			}
+		}
+		channel.Unlock()
+
+		// read from the remote track and write to the local track
+		buf := make([]byte, 1500)
+		for {
+			i, _, err := remoteTrack.Read(buf)
+			if err != nil {
+				return
+			}
+
+			if _, err = localTrack.Write(buf[:i]); err != nil {
+				return
+			}
+		}
+	})
+
+	// add all existing tracks to the current connecton
+	for _, track := range channel.Tracks {
+		for _, conn := range channel.Users {
+			if _, err := conn.AddTrack(track); err != nil {
+				panic(err)
+			}
+		}
 	}
 
-	err = peerConnection.SetRemoteDescription(offer)
+	err = peerConnection.SetRemoteDescription(*offer)
 	if err != nil {
 		panic(err)
 	}
